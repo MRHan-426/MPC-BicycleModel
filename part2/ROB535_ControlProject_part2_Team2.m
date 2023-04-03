0034abab29f4941a780ad0b02762a2d21dba9b89
SIYUAN YIN <128438562+Lynx1129@users.noreply.github.com>
2023-04-02 21:33:15 -0400

    Add files via upload

--- part2/ROB535_ControlProject_part2_Team2.m
+++ part2/ROB535_ControlProject_part2_Team2.m
@@ -24,8 +24,8 @@ g=9.806;
 track = TestTrack;
 l1 = size(track.cline,2);
 x_orig = linspace(1,10,l1);
-x_interp = linspace(1,10,l1*20); %previously X20
-r1 = interp1(x_orig,track.cline(1,:),x_interp); %
+x_interp = linspace(1,10,l1*20);
+r1 = interp1(x_orig,track.cline(1,:),x_interp);
 r2 = interp1(x_orig,track.cline(2,:),x_interp);
 track.cline = [r1;r2];
 r1 = interp1(x_orig,track.bl(1,:),x_interp);
@@ -35,7 +35,6 @@ r1 = interp1(x_orig,track.br(1,:),x_interp);
 r2 = interp1(x_orig,track.br(2,:),x_interp);
 track.br = [r1;r2];
 
-
 %number of states and inputs in dynamic model
 nstates=6;
 ninputs=2;
@@ -44,36 +43,26 @@ ninputs=2;
 input_range=[-0.499,   0.499;...
             -4999,4999]; 
 
-%load('project_traj_01_timestepfast_centerline.mat'); %loads Eric's fast trajectory
-%load('project_traj_01_timestepfast.mat'); %loads Eric's fast trajectory
-%load('project_traj_01_timestepslow.mat'); %loads Nikhil's slow trajectory
-%load('project_traj_01_timestepfast_centerline2.mat'); %loads Meet's stable trajectory
-
 %%
 persistent U_ref Y_ref
-% U_ref = control input
-% Y_ref = vehicle state, initial condition = (14)
 
 if isempty(U_ref)
-    %load('ROB535_ControlProject_part1_Team2.mat', 'U', 'Y');
-    %load('project_traj_01_timestep_centerline_ND.mat', 'U', 'Y');
-    load('trajectories/RefTraj.mat', 'U', 'Y');
-    U_ref = U'; %already 0.01 timestep
-    Y_ref = Y'; %already 0.01 timestep
+    load('part1_track.mat', 'U', 'Y');
+    U_ref = U';
+    Y_ref = Y';
 end
 
-dt=0.01;  % time discretization
+dt=0.01;
 persistent T
 if isempty(T)
     len1 = size(Y_ref,2);
     T = 0:dt:dt*(len1-1); %time span
 end
 
-%%
+%% Setting up the dynamics and partials
 persistent dfdx dfdu
 
 if isempty(dfdx)
-    %Setting up the dynamics and partials
     syms x u y v phi r Fx delta_f
     % x u y v phi r
     %slip angle functions in degrees
@@ -90,16 +79,6 @@ if isempty(dfdx)
     F_zr=a/(a+b)*m*g; %(4)
     F_yr=F_zr*Dy*sin(Cy*atan(By*phi_yr))+Svy; %(5)
 
-    % F_total=sqrt((Nw*Fx)^2+(F_yr^2)); %(10)
-    % F_max=0.7*m*g; %(11)
-
-    % if F_total>F_max
-    %     
-    %     Fx=F_max/F_total*Fx; %(12)
-    %   
-    %     F_yr=F_max/F_total*F_yr; %(13)
-    % end
-
     %vehicle dynamics  (1)
     % x u y v phi r
     f1 = u*cos(phi)-v*sin(phi);
@@ -109,30 +88,18 @@ if isempty(dfdx)
     f5 = r;
     f6 = (F_yf*a*cos(delta_f)-F_yr*b)/Iz;
      
-    % state = [x u y v phi r]
-    dfdx = [diff(f1,x)   diff(f1,u)   diff(f1,y)   diff(f1,v)   diff(f1,phi)   diff(f1,r)
-            diff(f2,x)   diff(f2,u)   diff(f2,y)   diff(f2,v)   diff(f2,phi)   diff(f2,r)
-            diff(f3,x)   diff(f3,u)   diff(f3,y)   diff(f3,v)   diff(f3,phi)   diff(f3,r)
-            diff(f4,x)   diff(f4,u)   diff(f4,y)   diff(f4,v)   diff(f4,phi)   diff(f4,r)
-            diff(f5,x)   diff(f5,u)   diff(f5,y)   diff(f5,v)   diff(f5,phi)   diff(f5,r)
-            diff(f6,x)   diff(f6,u)   diff(f6,y)   diff(f6,v)   diff(f6,phi)   diff(f6,r)];
+    %state = [x u y v phi r]
+    dfdx = jacobian([f1,f2,f3,f4,f5,f6],[x,u,y,v,phi,r]);
     
     % input  = [delta_f Fx]
     % delta_f = front wheel steering angle; 
     % Fx = traction force generated at each tire by the vehicle’s motor;
-    dfdu = [diff(f1,delta_f) diff(f1,Fx)
-            diff(f2,delta_f) diff(f2,Fx)
-            diff(f3,delta_f) diff(f3,Fx)
-            diff(f4,delta_f) diff(f4,Fx)
-            diff(f5,delta_f) diff(f5,Fx)
-            diff(f6,delta_f) diff(f6,Fx)];
-
+    dfdu = jacobian([f1,f2,f3,f4,f5,f6],[delta_f,Fx]);
 
     %these are the system linearized in discrete time about the reference
     %trajectory i.e. x(i+1)=A_i*x_i+B_i*u_i
     dfdx = matlabFunction(dfdx);
     dfdu = matlabFunction(dfdu);
-
 end
 
 
@@ -140,12 +107,9 @@ A=@(i) eye(nstates) + dt*dfdx(U_ref(1,i),Y_ref(5,i),Y_ref(6,i),Y_ref(2,i),Y_ref(
 B=@(i) dt*dfdu(U_ref(1,i),Y_ref(6,i),Y_ref(2,i),Y_ref(4,i));
 
 %%
-%11 timesteps for 3 states, 10 timesteps for 2 inputs
-npred=20;
-Ndec=(npred+1)*nstates + ninputs*npred;
-
 %decision variable will be z=[x_1...x_11;u_1...u_10] 
 % (x refers to state vector, u refers to input vector)
+npred=20;
 
 batch = 0.5/dt+1;
 
@@ -164,34 +128,30 @@ eY=NaN(nstates,batch);
 %set initial condition
 Y(:,1)=curr_state;
 
-%find start point
-diffs2 = (Y_ref - curr_state).^2;
-sum_diffs2 = diffs2(1,:) + diffs2(3,:);
-[~, t_index] = min(sum_diffs2);
+%calculate euclidean distance
+euc_dis = (Y_ref(1,:)-curr_state(1,:)).^2 + (Y_ref(3,:)-curr_state(3,:)).^2;
+[~, t_index] = min(euc_dis);
 
 FLAG_terminate = 0;
 
 %%
-%for i=t_index:min(t_index+batch-1, length(T)-1)
-for local_i=1:batch
-    diffs2 = (Y_ref(:,t_index:min(t_index+batch*2, size(Y_ref, 2))) - Y(:,local_i)).^2;
-    sum_diffs2 = diffs2(1,:) + diffs2(3,:);
-    [~, j] = min(sum_diffs2);
-    %i = t_index + j - 1;
-    i = local_i + t_index - 1;
+% tic
+for j=1:batch
+
+    i = j + t_index - 1;
     
     %shorten prediction horizon if we are at the end of trajectory
     npred_i=min([npred,length(T)-i-1]);
     
     %calculate error in states(actual - reference)
-    eY(:,local_i)=Y(:,local_i)-Y_ref(:,i);
+    eY(:,j)=Y(:,j)-Y_ref(:,i);
 
     %generate equality constraints
-    [Aeq,beq]=eq_cons(i,A,B,eY(:,local_i),npred_i,nstates,ninputs);
+    [Aeq,beq]=eq_cons(i,A,B,eY(:,j),npred_i,nstates,ninputs);
     
     %generate inequality constraints
-    car = [Y(1,local_i); Y(3,local_i)];
-    [A_con,b_con] = find_constraints(car,track,Xobs_seen);
+    vehicle = [Y(1,j); Y(3,j)];
+    [A_con,b_con] = build_constraints(vehicle,track,Xobs_seen);
     
     %generate boundary constraints
     [Lb,Ub]=bound_cons(i,U_ref,npred_i,input_range,nstates,ninputs);
@@ -205,17 +165,16 @@ for local_i=1:batch
     H=diag([repmat(Q,[1,npred_i+1]),repmat(R,[1,npred_i])]);
     
     func=zeros(nstates*(npred_i+1)+ninputs*npred_i,1);
+
     options = optimoptions('quadprog', 'Display', 'off');
     while 1
         % inequality constraints
         [Aineq,bineq]=ineq_cons(A_con,b_con, npred_i,nstates,ninputs);
-        horizon_ref = [reshape(Y_ref(:,i:i+npred_i),1,[]), zeros(1, npred_i*ninputs)];
-        bineq = bineq - Aineq*horizon_ref';
+        bineq = bineq - Aineq*[reshape(Y_ref(:,i:i+npred_i),1,[]), zeros(1, npred_i*ninputs)]';
         % use QP to solve the problem
-        [x,fval,exitflag,output] = quadprog(H,func,Aineq,bineq,Aeq,beq,Lb,Ub,[],options);
+        [x,~,exitflag,~,~] = quadprog(H,func,Aineq,bineq,Aeq,beq,Lb,Ub,[],options);
         if exitflag == -2
             n = size(A_con, 1);
-%             disp(['reducing constraints to ' num2str(n-1)]);
             A_con = A_con(1:n-1,:);
             b_con = b_con(1:n-1,:);
             if n <= 2
@@ -237,18 +196,20 @@ for local_i=1:batch
     end
     
     %get linearized input
-    u_mpc(:,local_i)=x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs);
+    u_mpc(:,j)=x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs);
     
     %get input
-    U(:,local_i)=u_mpc(:,local_i)+U_ref(:,i);
+    U(:,j)=u_mpc(:,j)+U_ref(:,i);
     
     %simulate model
-    [~,ztemp]=ode45(@(t,z)kinematic_bike_dynamics(t,z,U(:,local_i),0,Nw,f,Iz,a,b,By,Cy,Dy,Ey,Shy,Svy,m,g),[0 dt],Y(:,local_i));
+    [~,y]=ode45(@(t,z)kinematic_bike_dynamics(t,z,U(:,j),0,Nw,f,Iz,a,b,By,Cy,Dy,Ey,Shy,Svy,m,g),[0 dt],Y(:,j));
     
     %store final state
-    Y(:,local_i+1)=ztemp(end,:)';
+    Y(:,j+1)=y(end,:)';
     disp(['Done loop ' num2str(i) ' out of ' num2str(length(T)-1)])
+
 end
+%toc;
 
 %%
 sol_2 = U';
@@ -261,13 +222,9 @@ if FLAG_terminate == 1
     clear ROB535_ControlProject_part2_Team2;
 end
 
-
 end
 
 %% constraints.m
-% Creates linear inequality in the form of A*x < b
-% The constraint corresponds to all points to the left of the line traced
-% FROM p1 TO p2
 function [A, b] = constraint(p1, p2)
 
 d = p1 - p2;
@@ -336,13 +293,13 @@ end
 
 end
 
-%% find_constraints.m
-function [A, b] = find_constraints(car, track, obstacles)
+%% build_constraints.m
+function [A, b] = build_constraints(vehicle, track, obstacles)
 
-% Find the closest point along the lines to the car
-[left_i, left_j] = nearest_points(track.bl, car);
-[right_i, right_j] = nearest_points(track.br, car);
-[center_i, center_j] = nearest_points(track.cline, car);
+% Find the closest point along the lines to the vehicle
+[left_i, left_j] = nearest_points(track.bl, vehicle);
+[right_i, right_j] = nearest_points(track.br, vehicle);
+[center_i, ~] = nearest_points(track.cline, vehicle);
 
 % Create constraints from the sides of the road
 [A_left, b_left] = constraint(track.bl(:,left_j), track.bl(:,left_i));
@@ -357,7 +314,7 @@ for o = 1:n_obs
     obstacle_order(o) = nearest_points(track.cline, obstacles{o}(1, :)');
 end
 
-% Find the next obstacle in front of the car
+% Find the next obstacle in front of the vehicle
 inds = 1:n_obs;
 in_front = inds(obstacle_order > center_i - 2);
 if any(in_front)
@@ -366,20 +323,20 @@ if any(in_front)
     
     for o = next_obs
         % Only if we are within a certain distance of the obstacle
-        if sum((obstacles{o}(1,:)' - car).^2)^0.5 < 30
+        if sum((obstacles{o}(1,:)' - vehicle).^2)^0.5 < 30
             % Choose a side to pass the obstacle on
             [left_clearance, obs_left] = clearance(track.bl, obstacles{o});
             [right_clearance, obs_right] = clearance(track.br, obstacles{o});
 
             if left_clearance > right_clearance
                 for ob_i = 1:size(obs_left, 2)
-                    [A_obs, b_obs] = constraint(car, obs_left(:,ob_i));
+                    [A_obs, b_obs] = constraint(vehicle, obs_left(:,ob_i));
                     A = [A; A_obs];
                     b = [b; b_obs];
                 end
             else
                 for ob_i = 1:size(obs_right, 2)
-                    [A_obs, b_obs] = constraint(obs_right(:,ob_i), car);
+                    [A_obs, b_obs] = constraint(obs_right(:,ob_i), vehicle);
                     A = [A; A_obs];
                     b = [b; b_obs];
                 end
@@ -467,12 +424,11 @@ end
 %% nearest_points.m
 function [i, j] = nearest_points(line, p)
 
-diffs = line - p;
-dists_2 = diffs(1,:).^2 + diffs(2,:).^2;
-[~, inds] = mink(dists_2, 2);
+euc_dis = (line(1,:) - p(1,:)).^2 + (line(2,:) - p(2,:)).^2;
+[~, t_index] = mink(euc_dis, 2);
 
-i = inds(1);
-j = inds(2);
+i = t_index(1);
+j = t_index(2);
 
 if j < i
     [i, j] = deal(j, i);
@@ -481,26 +437,23 @@ end
 end
 
 %% clearance.m
-% Returns the clearance between obst and line and the point o on obst
-% which is closest to the line
-function [d, os] = clearance(line, obst)
+function [d, obstacle] = clearance(line, obs)
 
 closest_inds = [
-    nearest_points(line, obst(1, :)');
-    nearest_points(line, obst(2, :)');
-    nearest_points(line, obst(3, :)');
-    nearest_points(line, obst(4, :)');
+    nearest_points(line, obs(1, :)');
+    nearest_points(line, obs(2, :)');
+    nearest_points(line, obs(3, :)');
+    nearest_points(line, obs(4, :)');
 ];
 
-diffs = line(:, closest_inds) - obst';
+diffs = line(:, closest_inds) - obs';
 dists_2 = diffs(1,:).^2 + diffs(2,:).^2;
 
 [ds, is] = mink(dists_2, 2);
 d = min(ds).^0.5;
-%o = obst(i,:)';
-os = [];
+obstacle = [];
 for p = [-0.05, 0.01, 0.15]
-    os = [os (1-p)*obst(is,:)' + p*line(:, closest_inds(is))];
+    obstacle = [obstacle (1-p)*obs(is,:)' + p*line(:, closest_inds(is))];
 end
 
 end
